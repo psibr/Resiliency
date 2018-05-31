9fcd45859a5473ff2dca29edce94501422e97499
Alex Bauer <FacetiousProgrammer+git@gmail.com>
2018-05-31 12:26:43 -0700

    Updated the documentation around the random number generator interface and default implementation.

--- src/Resiliency/BackoffStrategies/IRandomNumberGenerator.cs
+++ src/Resiliency/BackoffStrategies/IRandomNumberGenerator.cs
@@ -6,40 +6,39 @@ using System.Text;
 namespace Resiliency.BackoffStrategies
 {
     /// <summary>
-    /// TODO: Improve documentation.
+    /// An interface for a random number generator that loosely follows the methods provided by the System.Random class.
     /// </summary>
     [EditorBrowsable(EditorBrowsableState.Never)]
     public interface IRandomNumberGenerator
     {
         /// <summary>
-        /// Non-negative integer.
+        /// Returns a non-negative integer.
         /// </summary>
         int Next();
 
         /// <summary>
-        /// Non-negative integer between [0, maxValue)
+        /// Returns a non-negative integer between [0, maxValue)
         /// </summary>
         int Next(int maxValue);
 
         /// <summary>
-        /// Non-negative integer in [minValue, maxValue)
+        /// Returns a non-negative integer in [minValue, maxValue)
         /// </summary>
         int Next(int minValue, int maxValue);
 
         /// <summary>
-        /// Non-negative double in [minValue, maxValue)
+        /// Returns a non-negative floating-point in [minValue, maxValue)
         /// </summary>
         double Next(double minValue, double maxValue);
 
         /// <summary>
-        /// Fills given array with random bytes.
+        /// Fills a given array with random bytes.
         /// </summary>
         void NextBytes(byte[] buffer);
 
         /// <summary>
-        /// Returns floating-point between [0, 1)
+        /// Returns a floating-point between [0, 1)
         /// </summary>
-        /// <returns></returns>
         double NextDouble();
     }
 }
