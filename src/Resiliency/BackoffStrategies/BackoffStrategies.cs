8ed2fb32bd933078dcb901109c61ec2e8020c533
Alex Bauer <FacetiousProgrammer+git@gmail.com>
2018-05-31 12:09:12 -0700

    Added a static class to new up the basic backoff strategies.

---
+++ src/Resiliency/BackoffStrategies/BackoffStrategies.cs
@@ -0,0 +1,23 @@
+﻿using Resiliency.BackoffStrategies;
+using System;
+
+namespace Resiliency
+{
+    public static class Backoff
+    {
+        public static IBackoffStrategy Exponentially(TimeSpan initialWaitTime)
+        {
+            return new ExponentialBackoffStrategy(initialWaitTime);
+        }
+
+        public static IBackoffStrategy Linearlly(TimeSpan initialWaitTime)
+        {
+            return new LinearBackoffStrategy(initialWaitTime);
+        }
+
+        public static IBackoffStrategy Constantly(TimeSpan initialWaitTime)
+        {
+            return new ConstantBackoffStrategy(initialWaitTime);
+        }
+    }
+}
