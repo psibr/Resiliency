865461e9a1d2f9d6ac3de4c816cdfb7759389f20
Ovan Crone <ovan@psibr.net>
2018-06-01 17:57:41 -0700

    Change naming to feel more englishy ;)

--- src/Resiliency/BackoffStrategies/BackoffStrategies.cs
+++ src/Resiliency/BackoffStrategies/BackoffStrategies.cs
@@ -5,19 +5,19 @@ namespace Resiliency
 {
     public static class Backoff
     {
-        public static IBackoffStrategy Exponentially(TimeSpan initialWaitTime)
+        public static IBackoffStrategy ExponentiallyFrom(TimeSpan initialWaitTime)
         {
             return new ExponentialBackoffStrategy(initialWaitTime);
         }
 
-        public static IBackoffStrategy Linearlly(TimeSpan initialWaitTime)
+        public static IBackoffStrategy LinearlyFrom(TimeSpan initialWaitTime)
         {
             return new LinearBackoffStrategy(initialWaitTime);
         }
 
-        public static IBackoffStrategy Constantly(TimeSpan initialWaitTime)
+        public static IBackoffStrategy Constant(TimeSpan waitTime)
         {
-            return new ConstantBackoffStrategy(initialWaitTime);
+            return new ConstantBackoffStrategy(waitTime);
         }
     }
 }
